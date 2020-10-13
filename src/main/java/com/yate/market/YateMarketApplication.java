32c1ff4ac740ab08fda6d6947ec9042b3b3b3ebe
camilo Yate <cyateya35439@universidadean.edu.co>
2020-10-12 22:37:17 -0500

    Base de todo proyecto Spring

---
+++ src/main/java/com/yate/market/YateMarketApplication.java
@@ -0,0 +1,13 @@
+package com.yate.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YateMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YateMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yate/market/YateMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yate.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YateMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
